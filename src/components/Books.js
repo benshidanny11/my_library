192eaa603a2720451a937ee2c04fac2f0e58e2e4
Urimubenshi Daniel <benshidanny11@gmail.com>
2020-04-30 01:26:49 +0200

    jjjj

--- src/components/Books.js
+++ src/components/Books.js
@@ -5,7 +5,7 @@ function Books(props) {
         <div>
           <li className="card-body list-group-item">
           <img src={props.imageUrl} className="img-fluid list-group-item" alt="Responsive image"/>
-          <a href="#" className="list-group-item list-group-item-action flex-column align-items-center">
+          <a href={"/"} className="list-group-item list-group-item-action flex-column align-items-center">
            <div className="d-flex w-100 justify-content-between">
             <h5 className="mb-1">{props.title}</h5>
            </div>
